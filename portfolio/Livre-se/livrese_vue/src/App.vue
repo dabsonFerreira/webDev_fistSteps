81206264c7ce3f42ac4b3e0d69477dd1bb6032e7
Dabson Ferreira <ferreira.santos@engenharia.ufjf.br>
2022-05-28 18:41:47 -0300

    updating livre-se using vuejs and django

---
+++ portfolio/Livre-se/livrese_vue/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand"> 
+        <router-link to="/" class="navbar-item"><strong>Livre-se</strong></router-link>
+        
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-end">
+          <router-link to="/aventura" class="navbar-item">Aventura</router-link>
+          <router-link to="/terror"   class="navbar-item">Terror</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </nav>   
+    
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    data(){
+      return {
+        showMobileMenu: false,
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';  
+</style>
