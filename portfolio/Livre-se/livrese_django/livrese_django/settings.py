2769930823ead2dfc5f21411f20d0992bab8bbd3
Dabson Ferreira <ferreira.santos@engenharia.ufjf.br>
2022-05-28 23:11:55 -0300

    há algum erro de cors a ser resolvido

--- portfolio/Livre-se/livrese_django/livrese_django/settings.py
+++ portfolio/Livre-se/livrese_django/livrese_django/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
 
+    'product'
+
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -129,8 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
